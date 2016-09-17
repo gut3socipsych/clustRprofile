325f78bf9ebea0d0b8f4e92fa478a802897cfc25
Charles Guthrie <gut3.socipsych@gmail.com>
2016-09-17 17:04:37 -0400

    modified test document to develop class object within function

--- development_notes_temp.R
+++ development_notes_temp.R
@@ -1,44 +1,34 @@
 #WORKING NOTES....
-library(reshape2)
-source("clustRprofile_function_script.R")
 
-test <- mtcars
+#develop into class object with multiple outputs 
 
-test_dist <- dist(test,method = "e")
-test_clust <- hclust(test_dist)
-plot(test_clust)
-clust_assgn <- cutree(test_clust, k = 3)
-test_z <- as.data.frame(scale(x = test, center = T, scale = T))
-test_z <- cbind(test_z, as.data.frame(clust_assgn)) #attach cluster assignment 
-test_z <- aggregate(.~clust_assgn, data = test_z, FUN = mean) #summarize standarized scores df
-test_z<-melt(test_z, id.vars = "clust_assgn") #reshape standardized scores df 
-
-clustRprofile(data_melt = test_z)
-test$clust_assign <- clust_assgn
-
-
-
-a <- test_fun(test, 4)
-a
-clustRprofile(test, 2)
-#necessary steps to format original data 
-#standardized the values of each variable, 
-#so that they can be plotted uniformlly 
-
-#attach cluster assignment to the standarized data
-
-clustRprofile_v2<-function(data_melt){
-  require(ggplot2)
-  c_theme_default<-theme_gray() +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.25))
-  profile_plot<-ggplot(data_melt, aes(x = variable, y = value, group = as.factor(clust_assgn), color = as.factor(clust_assgn))) +
+clustRprofile <- function(dataset, cluster_count){
+  setClass(Class = "clustRprofile",
+           slots = c(cluster_tree_plot = "ANY", profile_plot = "ANY"))
+  require(ggplot2);require(reshape2)
+  #calculate cluster groups
+  df_dist <- dist(dataset, method = "e")
+  df_clust <- hclust(df_dist)
+  cluster_tree_plot <- plot(df_clust)
+  clust_assgn <- cutree(df_clust, k = cluster_count)
+  df_z <- as.data.frame(scale(x = dataset, center = T, scale = T))
+  df_z <- cbind(df_z, as.data.frame(clust_assgn))
+  df_z <- aggregate(.~clust_assgn, data = df_z, FUN = mean)
+  df_z <- melt(df_z, id.vars = "clust_assgn")
+  #build cluster plot
+  profile_plot <- ggplot(data = df_z, aes(x = variable, 
+                                          y = value, 
+                                          group = as.factor(clust_assgn), 
+                                          color = as.factor(clust_assgn))) +
     geom_path(alpha = 0.9) + geom_point() + 
-    labs(x = "Variables", y = "Scaled Averages", title = "Cluster Profiles", color = "Clusters") + 
-    c_theme_default
-  print(profile_plot)
+    labs(x = "Variables", y = "Scaled Averages", title = "Cluster Profiles", color = "Clusters") +
+    theme_gray() + theme(axis.text.x = element_text(angle = 90, vjust = 0.25))
+  return(new("clustRprofile",cluster_tree_plot=cluster_tree_plot ,profile_plot=profile_plot))
 }
 
-test_fun <- function(){
-  
-}
+test <- clustRprofile(dataset = mtcars, cluster_count = 3)
+
+
+test@profile_plot
+test@cluster_tree_plot ###fix this ... 
 
