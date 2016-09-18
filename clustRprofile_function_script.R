9f745536593cacd9a07c0b4a4d39c9972dffe41e
Charles Guthrie <gut3.socipsych@gmail.com>
2016-09-18 16:18:53 -0400

    updated function's class slots to include cluster outputs and cluster assigned data frame

--- clustRprofile_function_script.R
+++ clustRprofile_function_script.R
@@ -1,4 +1,8 @@
 clustRprofile <- function(dataset, cluster_count){
+  setClass(Class = "clustRprofile",
+           slots = c(profile_plot = "ANY",
+                     cluster_assigned_df = "data.frame",
+                     cluster_values = "ANY"))
   require(ggplot2);require(reshape2)
   #calculate cluster groups
   df_dist <- dist(dataset, method = "e")
@@ -8,6 +12,8 @@ clustRprofile <- function(dataset, cluster_count){
   df_z <- cbind(df_z, as.data.frame(clust_assgn))
   df_z <- aggregate(.~clust_assgn, data = df_z, FUN = mean)
   df_z <- melt(df_z, id.vars = "clust_assgn")
+  #build cluster assigned data frame 
+  cluster_assigned_df <- cbind(dataset, clust_assgn)
   #build cluster plot
   profile_plot <- ggplot(data = df_z, aes(x = variable, 
                                           y = value, 
@@ -16,6 +22,10 @@ clustRprofile <- function(dataset, cluster_count){
     geom_path(alpha = 0.9) + geom_point() + 
     labs(x = "Variables", y = "Scaled Averages", title = "Cluster Profiles", color = "Clusters") +
     theme_gray() + theme(axis.text.x = element_text(angle = 90, vjust = 0.25))
-  return(profile_plot)
+  return(new("clustRprofile",
+             profile_plot=profile_plot,
+             cluster_assigned_df=cluster_assigned_df,
+             cluster_values=df_clust))
 }
+
 
--- development_notes_temp.R
+++ development_notes_temp.R
@@ -4,17 +4,20 @@
 
 clustRprofile <- function(dataset, cluster_count){
   setClass(Class = "clustRprofile",
-           slots = c(cluster_tree_plot = "ANY", profile_plot = "ANY"))
+           slots = c(profile_plot = "ANY",
+                     cluster_assigned_df = "data.frame",
+                     cluster_values = "ANY"))
   require(ggplot2);require(reshape2)
   #calculate cluster groups
   df_dist <- dist(dataset, method = "e")
   df_clust <- hclust(df_dist)
-  cluster_tree_plot <- plot(df_clust)
   clust_assgn <- cutree(df_clust, k = cluster_count)
   df_z <- as.data.frame(scale(x = dataset, center = T, scale = T))
   df_z <- cbind(df_z, as.data.frame(clust_assgn))
   df_z <- aggregate(.~clust_assgn, data = df_z, FUN = mean)
   df_z <- melt(df_z, id.vars = "clust_assgn")
+  #build cluster assigned data frame 
+  cluster_assigned_df <- cbind(dataset, clust_assgn)
   #build cluster plot
   profile_plot <- ggplot(data = df_z, aes(x = variable, 
                                           y = value, 
@@ -23,12 +26,17 @@ clustRprofile <- function(dataset, cluster_count){
     geom_path(alpha = 0.9) + geom_point() + 
     labs(x = "Variables", y = "Scaled Averages", title = "Cluster Profiles", color = "Clusters") +
     theme_gray() + theme(axis.text.x = element_text(angle = 90, vjust = 0.25))
-  return(new("clustRprofile",cluster_tree_plot=cluster_tree_plot ,profile_plot=profile_plot))
+  return(new("clustRprofile",
+             profile_plot=profile_plot,
+             cluster_assigned_df=cluster_assigned_df,
+             cluster_values=df_clust))
 }
 
 test <- clustRprofile(dataset = mtcars, cluster_count = 3)
 
 
-test@profile_plot
-test@cluster_tree_plot ###fix this ... 
-
+test@profile_plot #main plot 
+test@cluster_values #hclust function output 
+plot(test@cluster_values) #plot for dendrogram 
+output_test_df <- test@cluster_assigned_df #cluster assignment attached to original data set 
+View(output_test_df)
